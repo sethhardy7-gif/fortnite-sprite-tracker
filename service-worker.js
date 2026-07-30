6788212fe4730460cedfcd827ccdab395f911697
sethhardy7-gif <sethhardy7@icloud.com>
2026-07-30 17:12:21 -0500

    Correct all Sprite image assignments

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='sprite-tracker-v3-101-images';
+const CACHE='sprite-tracker-v4-101-images';
 const ASSETS=['./','index.html','styles.css','app.js','catalog.json','manifest.webmanifest','icon.svg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
